7c61943973274180587c59623ed390cc91659f30
johnherry123 <97157371+johnherry123@users.noreply.github.com>
2023-02-02 21:13:04 +0700

    Create Account.java

---
+++ src/java/module/Account.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package module;
+
+/**
+ *
+ * @author xuank
+ */
+public class Account {
+    private String username;
+    private String password;
+    private String displayname;
+
+    public Account() {
+    }
+
+    public Account(String username, String password, String displayname) {
+        this.username = username;
+        this.password = password;
+        this.displayname = displayname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDisplayname() {
+        return displayname;
+    }
+
+    public void setDisplayname(String displayname) {
+        this.displayname = displayname;
+    }
+    
+    
+}
