827f83c1d0d457153528eb4893ecd44cd85c78bd
johnherry123 <97157371+johnherry123@users.noreply.github.com>
2023-02-02 21:53:47 +0700

    Update LoginEmpt.java

--- src/java/controller/LoginEmpt.java
+++ src/java/controller/LoginEmpt.java
@@ -4,10 +4,55 @@
  */
 package controller;
 
+import dal.AccountDAO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import module.Account;
+
 /**
  *
  * @author xuank
  */
-public class LoginEmpt {
-    
+public class LoginEmpt extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // super.doPost(req, resp); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+        String email = req.getParameter("email");
+        String password = req.getParameter("pass");
+        String err = "1";
+        if (email.isEmpty()) {
+            err = " Please input the your Username";
+
+        } else {
+            if (password.isEmpty()) {
+                err = "Please input the your Password";
+
+            }
+
+        }
+        if (err.equals("1") == false) {
+            req.setAttribute("err", err);
+            req.getRequestDispatcher("login-admin.jsp").forward(req, resp);
+        } else {
+            AccountDAO acc = new AccountDAO();
+            Account accout = acc.checkLoginCus(err, password);
+            if (accout == null) {
+                req.setAttribute("err", "Username is not exist");
+                req.getRequestDispatcher("login-admin.jsp").forward(req, resp);
+            }else{
+                req.getRequestDispatcher("#").forward(req, resp);
+            }
+        }
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
 }
