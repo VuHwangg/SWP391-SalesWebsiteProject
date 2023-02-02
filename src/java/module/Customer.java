f2089e7a5b7b27494bfd6f7cce7ff396c2f2139b
johnherry123 <97157371+johnherry123@users.noreply.github.com>
2023-02-02 10:24:42 +0700

    Create Customer.java

---
+++ src/java/module/Customer.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package module;
+
+/**
+ *
+ * @author tient
+ */
+public class Customer {
+    private int customerId;
+    private String customerName;
+    private String address;
+    private String phone;
+    private String email;
+
+    public Customer() {
+    }
+
+    public Customer(int customerId, String customerName, String address, String phone, String email) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+}
