526562fe7ff77e8a7ff9d28c12219dfcb6722ece
johnherry123 <97157371+johnherry123@users.noreply.github.com>
2023-02-02 10:31:39 +0700

    Update Constants.java

--- src/java/module/Constants.java
+++ src/java/module/Constants.java
@@ -11,7 +11,7 @@ package module;
 public class Constants {
   public static String GOOGLE_CLIENT_ID = "1085689904569-u4662st25sdv1a20rngoqc2vn1ucibrh.apps.googleusercontent.com";
   public static String GOOGLE_CLIENT_SECRET = "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";
-  public static String GOOGLE_REDIRECT_URI = "http://localhost:9999/LoginGoogle/login-google";
+  public static String GOOGLE_REDIRECT_URI = "http://localhost:9999/SWP391-SalesWebsiteProject/LoginUser";
   public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
   public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
   public static String GOOGLE_GRANT_TYPE = "authorization_code";
