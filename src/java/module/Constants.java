ce26f35724158303c75331955d5e15e636c5b313
johnherry123 <97157371+johnherry123@users.noreply.github.com>
2023-02-02 10:24:40 +0700

    Create Constants.java

---
+++ src/java/module/Constants.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package module;
+
+/**
+ *
+ * @author tient
+ */
+public class Constants {
+  public static String GOOGLE_CLIENT_ID = "1085689904569-u4662st25sdv1a20rngoqc2vn1ucibrh.apps.googleusercontent.com";
+  public static String GOOGLE_CLIENT_SECRET = "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";
+  public static String GOOGLE_REDIRECT_URI = "http://localhost:9999/LoginGoogle/login-google";
+  public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+  public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+  public static String GOOGLE_GRANT_TYPE = "authorization_code";
+}
