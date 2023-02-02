fe637d23af2e4751667484ccc4192c4615fda7bf
johnherry123 <97157371+johnherry123@users.noreply.github.com>
2023-02-02 10:24:39 +0700

    Create DBContext.java

---
+++ src/java/dal/DBContext.java
@@ -0,0 +1,27 @@
+
+package dal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DBContext {
+    protected Connection connection;
+    public DBContext()
+    {
+        try {
+            // Edit URL , username, password to authenticate with your MS SQL Server
+            String url = "jdbc:sqlserver://localhost:1433;databaseName= SWP";
+            String username = "sa";
+            String password = "12345678";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(new DBContext().connection);
+    }
+}
