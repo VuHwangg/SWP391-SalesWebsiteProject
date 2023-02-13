0b226fcabe24f96d667d01340ec284fadf2a714f
NVA <vieetanhnguyeenx@gmail.com>
2023-02-13 21:21:28 +0700

    Update Cart

--- src/java/model/Cart.java
+++ src/java/model/Cart.java
@@ -16,6 +16,39 @@ public class Cart {
 
     public Cart() {
     }
+
+    public int getCartId() {
+        return cartId;
+    }
+
+    public void setCartId(int cartId) {
+        this.cartId = cartId;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
     
     
 }
