600d9859d1db217398cc5f0b5893ee89e62dee3f
NxTGiang <kidsno95@gmail.com>
2023-02-17 01:05:28 +0700

    update srs

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -14,8 +14,8 @@ public class DBContext {
             // Edit URL , username, password to authenticate with your MS SQL Server
 
 
-            String user = "sa";
-            String pass = "12345678";
+            String user = "giangnxthe161756";
+            String pass = "tifver55";
 
 
 
