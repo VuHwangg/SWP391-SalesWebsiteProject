e4ddb29715157f49669a81a778a2f29c6d1fec2f
johnherry123 <97157371+johnherry123@users.noreply.github.com>
2023-02-17 15:33:25 +0700

    Update DBContext.java

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -14,17 +14,12 @@ public class DBContext {
             // Edit URL , username, password to authenticate with your MS SQL Server
 
 
-<<<<<<< HEAD
-            String user = "giangnxthe161756";          
-            String pass = "tifver55";
-=======
 
 
             String user = "sa";
             String pass = "12345678";
 
 
->>>>>>> 27a280c4c7644efde1e8a08c20d5f2087dfb9ec8
 
 
             String url = "jdbc:sqlserver://localhost:1433;databaseName=SWP";
