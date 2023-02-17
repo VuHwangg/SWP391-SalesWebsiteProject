acf14f8e3fbf8b71a6efd3ab3ac078145d4abd2d
johnherry123 <97157371+johnherry123@users.noreply.github.com>
2023-02-17 17:27:42 +0700

    Update DBContext.java

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -13,9 +13,6 @@ public class DBContext {
         try {
             // Edit URL , username, password to authenticate with your MS SQL Server
 
-<<<<<<< Updated upstream
-
-
 
             String user = "sa";
             String pass = "12345678";
@@ -23,10 +20,6 @@ public class DBContext {
 
 
 
-=======
-            String user = "sa";
-            String pass = "sa";
->>>>>>> Stashed changes
             String url = "jdbc:sqlserver://localhost:1433;databaseName=SWP";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
