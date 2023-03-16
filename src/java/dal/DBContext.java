636669cd9f5aee962133f02820cb26da0e25742d
NxTGiang <kidsno95@gmail.com>
2023-03-16 14:16:22 +0700

    Update DBContext.java

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -13,8 +13,8 @@ public class DBContext {
         try {
             // Edit URL , username, password to authenticate with your MS SQL Server
             String url = "jdbc:sqlserver://localhost:1433;databaseName= SWP";
-            String username = "sa";
-            String password = "sa";
+            String username = "giangnxthe161756";
+            String password = "tifver55";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, username, password);
         } catch (ClassNotFoundException | SQLException ex) {
