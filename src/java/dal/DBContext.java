21ffc3343250cdf4c01ed80f941ab3314c2203b7
NxTGiang <kidsno95@gmail.com>
2023-03-20 19:25:10 +0700

    update doc

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -13,9 +13,9 @@ public class DBContext {
         try {
             // Edit URL , username, password to authenticate with your MS SQL Server
             String url = "jdbc:sqlserver://localhost:1433;databaseName= SWP";
-            String username = "sa";
+            String username = "giangnxthe161756";
 
-            String password = "12345678";
+            String password = "tifver55";
 
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, username, password);
