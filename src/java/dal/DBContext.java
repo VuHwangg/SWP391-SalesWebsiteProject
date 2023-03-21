b87afb843ed03e56bf5cd88d6f59eec343f479bd
NVA <vieetanhnguyeenx@gmail.com>
2023-03-21 20:56:56 +0700

    Update DBContext

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -13,17 +13,9 @@ public class DBContext {
         try {
             // Edit URL , username, password to authenticate with your MS SQL Server
             String url = "jdbc:sqlserver://localhost:1433;databaseName= SWP";
-            String username = "duyct";
+            String username = "sa";
 
-<<<<<<< Updated upstream
-            String password = "1312002ss";
-=======
-<<<<<<< HEAD
             String password = "sa";
-=======
-            String password = "1312002ss";
->>>>>>> 25b064ae9c6c765a5b12b23bb0076063a589c2c2
->>>>>>> Stashed changes
 
 
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
