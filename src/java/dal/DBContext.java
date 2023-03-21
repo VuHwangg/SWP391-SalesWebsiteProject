6b06ba633a7464bae373e8a8bb4378da41a1f532
johnherry123 <97157371+johnherry123@users.noreply.github.com>
2023-03-21 23:21:48 +0700

    .

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -1,42 +1,42 @@
-
 package dal;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 
+/**
+ *
+ * @author FPT University - PRJ301
+ */
 public class DBContext {
     protected Connection connection;
     public DBContext()
     {
+        //@Students: You are allowed to edit user, pass, url variables to fit 
+        //your system configuration
+        //You can also add more methods for Database Interaction tasks. 
+        //But we recommend you to do it in another class
+        // For example : StudentDBContext extends DBContext , 
+        //where StudentDBContext is located in dal package, 
         try {
-            // Edit URL , username, password to authenticate with your MS SQL Server
-            String url = "jdbc:sqlserver://localhost:1433;databaseName= SWP";
-<<<<<<< Updated upstream
-
-            String username = "duyct";
-=======
-            String username = "sa";
-<<<<<<< HEAD
-            String password = "30082002Xyz@";
-=======
->>>>>>> Stashed changes
-
-
-
-            String password = "1312002ss";
-
-
-
-
->>>>>>> 5241a7a01c763fa095710f69dd3e95637d779fc2
+            String user = "sa";
+            String pass = "12345678";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=SWP";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            connection = DriverManager.getConnection(url, username, password);
+            connection = DriverManager.getConnection(url, user, pass);
         } catch (ClassNotFoundException | SQLException ex) {
-            System.out.println(ex);
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    
+    // Kiem tra ket noi
     public static void main(String[] args) {
         System.out.println(new DBContext().connection);
     }
