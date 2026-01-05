bd73c1cbe599314db684975919453df66882bdad
Hoàng Chu Anh Vũ <95555537+VuHwangg@users.noreply.github.com>
2026-01-05 15:11:21 +0700

    Update SQL queries to use double quotes for table names

--- src/java/dal/BrandDBContext.java
+++ src/java/dal/BrandDBContext.java
@@ -23,10 +23,10 @@ public class BrandDBContext extends DBContext {
     public ArrayList<Brand> listByID(int product_id) {
         ArrayList<Brand> brands = new ArrayList<>();
         try {
-            // Loại bỏ dấu ngoặc vuông []
+            // Sửa: Thêm ngoặc kép cho tên bảng \"Brand\" và \"Product_Brand\"
             String sql = "SELECT b.brand_id, b.brand_name, b.description "
-                    + " FROM Brand b "
-                    + " INNER JOIN Product_Brand pb ON pb.brand_id = b.brand_id "
+                    + " FROM \"Brand\" b "
+                    + " INNER JOIN \"Product_Brand\" pb ON pb.brand_id = b.brand_id "
                     + " WHERE pb.product_id = ?";
             PreparedStatement stm = connection.prepareStatement(sql);
             stm.setInt(1, product_id);
@@ -50,11 +50,11 @@ public class BrandDBContext extends DBContext {
     public ArrayList<Brand> listByType(int type) {
         ArrayList<Brand> brands = new ArrayList<>();
         try {
-            // Loại bỏ dấu ngoặc vuông []
+            // Sửa: Thêm ngoặc kép cho \"Brand\", \"Product_Brand\", \"Product\"
             String sql = "SELECT DISTINCT b.brand_id, b.brand_name, b.description "
-                    + " FROM Brand b "
-                    + " INNER JOIN Product_Brand pb ON pb.brand_id = b.brand_id "
-                    + " INNER JOIN Product pr ON pr.product_id = pb.product_id "
+                    + " FROM \"Brand\" b "
+                    + " INNER JOIN \"Product_Brand\" pb ON pb.brand_id = b.brand_id "
+                    + " INNER JOIN \"Product\" pr ON pr.product_id = pb.product_id "
                     + " WHERE pr.type = ?";
             PreparedStatement stm = connection.prepareStatement(sql);
             stm.setInt(1, type);
@@ -78,8 +78,8 @@ public class BrandDBContext extends DBContext {
     public List<Brand> getAllBrand() {
         List<Brand> brands = new ArrayList<>();
         try {
-            // Loại bỏ dấu ngoặc vuông []
-            String sql = "SELECT brand_id, brand_name, description FROM Brand";
+            // Sửa: Thêm ngoặc kép \"Brand\"
+            String sql = "SELECT brand_id, brand_name, description FROM \"Brand\"";
             PreparedStatement stm = connection.prepareStatement(sql);
             ResultSet rs = stm.executeQuery();
             while (rs.next()) {
@@ -100,8 +100,8 @@ public class BrandDBContext extends DBContext {
 
     public int AddNewBrandAndReturnId(String brandName) {
         try {
-            // Loại bỏ [dbo]. và []
-            String sql = "INSERT INTO Brand (brand_name) VALUES (?)";
+            // Sửa: Thêm ngoặc kép \"Brand\"
+            String sql = "INSERT INTO \"Brand\" (brand_name) VALUES (?)";
             
             // PostgreSQL hỗ trợ Statement.RETURN_GENERATED_KEYS thông qua JDBC driver rất tốt
             PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
@@ -110,6 +110,7 @@ public class BrandDBContext extends DBContext {
             
             ResultSet rs = ps.getGeneratedKeys();
             if (rs.next()) {                
+                // Cột trả về mặc định của Postgres thường là cột đầu tiên hoặc cột ID
                 return rs.getInt(1);
             }
         } catch (SQLException ex) {
